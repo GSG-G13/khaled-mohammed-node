5d6b9ed678150147fdb34ee1907181cb9518912f
KhaledToha <khaled.f.toha@gmail>
2023-03-22 00:54:01 +0200

    adding front-end files and functionality Relate #4

---
+++ public/script/script1.js
@@ -0,0 +1,56 @@
+const searchForm = document.querySelector("#search-form");
+const cardList = document.querySelector(".result-list");
+const searchInput = document.querySelector("#search-input");
+const api = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  cardList.innerHTML = "";
+  getData("GET", api + searchInput.value, (data) => {
+    data.forEach((element) => {
+      let card = document.createElement("li");
+      card.classList.add("result-card");
+
+      let header = document.createElement("div");
+      header.classList.add("header");
+
+      let title = document.createElement("span");
+      title.classList.add("title");
+      title.textContent = element.word;
+
+      let sound = document.createElement("span");
+      sound.classList.add("sound");
+      sound.textContent = element.phonetic;
+
+      header.appendChild(title);
+      header.appendChild(sound);
+
+      let meaning = document.createElement("div");
+      meaning.classList.add("meaning");
+
+      element.meanings.forEach((item) => {
+        let partOfSpeech = document.createElement("span");
+        partOfSpeech.classList.add("part-of-speech");
+        partOfSpeech.textContent = item.partOfSpeech;
+
+        let definitionList = document.createElement("ul");
+        definitionList.classList.add("definition-list");
+
+        item.definitions.forEach((def) => {
+          let definition = document.createElement("li");
+          definition.classList.add("definition");
+          definition.textContent = def.definition;
+
+          definitionList.appendChild(definition);
+        });
+
+        meaning.appendChild(partOfSpeech);
+        meaning.appendChild(definitionList);
+      });
+
+      card.appendChild(header);
+      card.appendChild(meaning);
+      cardList.appendChild(card);
+    });
+  });
+});
