920f2761fa7244490cfaa15ebb2027b26a08aae7
KhaledToha <khaled.f.toha@gmail>
2023-03-22 14:20:17 +0200

    adding autoComplete function Rekate #8

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const router = require('./router');
 const port = process.env.PORT || 4300;;
 const hostName = 'localhost';
  const server = http.createServer(router);
+ console.log('server')
 server.listen(port , hostName , ()=>{
     console.log(`Server running at port http://${hostName}:${port}`);
 } );
